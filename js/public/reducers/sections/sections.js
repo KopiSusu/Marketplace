5d824928240700321d83398bf22c6dbb4641214c
adelbalso <a.delbalso@gmail.com>
2016-09-23 15:59:38 -0400

    ports and copy

--- js/public/reducers/sections/sections.js
+++ js/public/reducers/sections/sections.js
@@ -7,7 +7,7 @@ const sections = [
   {
     "nestedKey" : "topText",
     "type" : "contentOnly",
-    "text" : "Homemade is a <style>curated marketplace for small batch producers.<style> Enjoy some of these delicious cookies made by one of our fantastic chefs.",
+    "text" : "Bed Stuy Eats is a <style>curated marketplace for small batch producers.<style> Enjoy some of these delicious cookies made by one of our fantastic chefs.",
     "style": {
       "fontSize": "22px",
       "lineHeight": "1.5"
