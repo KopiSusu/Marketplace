700cb6461fb3ea1dcfbf734222b7bbd26a3e970a
adelbalso <a.delbalso@gmail.com>
2016-10-04 16:01:19 -0400

    more featured page

--- js/public/view-web/scene/core/sections/components/temp/temp.js
+++ js/public/view-web/scene/core/sections/components/temp/temp.js
@@ -2,6 +2,11 @@
 import React from 'react'
 import _ from 'lodash'
 import Video from 'react-html5video'
+import Blob from './../view/components/blob'
+import Cards from './../view/components/cards'
+import Profiles from './../view/components/profiles'
+import Marker from './../view/components/marker'
+import Content from './../view/components/content'
 
 import './temp.css'
 
@@ -26,7 +31,42 @@ class Temp extends React.Component {
 
                 <section className='section column-1 featured'>
 
-                    <h1>TEMP!!</h1>
+                    <h1>Staff picks</h1>
+
+                    <ul>
+                        <li className="cards column-3">
+                            <div className='card'>
+                                <div className='image rows-6-5' style={{backgroundImage: `url("https://s3.amazonaws.com/homemade-user-uploads/0neH9RMVHs")`}} > 
+                                </div>
+                                <div className="namePrice">
+                                    <p className='column-6-5 name'>15 Brown Butter Hazelnut - SPECIAL - For Local Youth Charity VARIETY</p>
+                                    <p className='column-6-1 price'>$22</p>
+                                </div>
+                                {
+                                    <div className='cook'>
+                                        <div className="profileImage" style={{backgroundImage: `url("https://s3.amazonaws.com/homemade-user-uploads/TGOJfh4bid")`}} ></div>
+                                        <p className="cookName">Gooey and Co</p>
+                                    </div>
+                                }
+                            </div>
+                        </li>
+                        <li className="cards column-3">
+                            <div className='card'>
+                                <div className='image rows-6-5' style={{backgroundImage: `url("https://s3.amazonaws.com/homemade-user-uploads/0neH9RMVHs")`}} > 
+                                </div>
+                                <div className="namePrice">
+                                    <p className='column-6-5 name'>15 Brown Butter Hazelnut - SPECIAL - For Local Youth Charity VARIETY</p>
+                                    <p className='column-6-1 price'>$22</p>
+                                </div>
+                                {
+                                    <div className='cook'>
+                                        <div className="profileImage" style={{backgroundImage: `url("https://s3.amazonaws.com/homemade-user-uploads/TGOJfh4bid")`}} ></div>
+                                        <p className="cookName">Gooey and Co</p>
+                                    </div>
+                                }
+                            </div>
+                        </li>
+                    </ul>
 
                     <Video className='video rows-6-5' controls loop muted >
                         <source src={"http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_h264.mov"} type="video/webm" />
