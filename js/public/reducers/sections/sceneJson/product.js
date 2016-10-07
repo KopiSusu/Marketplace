090408b5104504c4c1757e4b1042dbbbfe7b1b01
Komang Luce <komangluce@koh.local>
2016-10-07 11:41:32 -0400

    small styling tweak to fix ovrlap

--- js/public/reducers/sections/sceneJson/product.js
+++ js/public/reducers/sections/sceneJson/product.js
@@ -3,7 +3,6 @@ export default [
     "nestedKey" : "meal",
     "type" : "selected",
     "class" : "selected",
-    "search": true,
     "requestType": "Meal",
     "noNavigation": true,
     "queries": [
